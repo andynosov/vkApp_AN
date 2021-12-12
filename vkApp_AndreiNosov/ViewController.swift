bd7b2e3c9f5cd00699eb25d9fb1cc2ac7a1c3538
andynosov <andynosov@gmail.com>
2021-12-12 16:32:29 +0800

    Initial Commit

---
+++ vkApp_AndreiNosov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vkApp_AndreiNosov
+//
+//  Created by Andrei Nosov on 12.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
